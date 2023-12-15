d0e690bdae0a71dbc9b1bbc346932b2af97bcf8a
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-15 23:47:14 +0900

    feat: add button show more

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,13 +7,14 @@ import Main from '../Main'
 import Tabs from '../Tabs'
 import CardList from '../CardList'
 import { fetchSearchId, fetchTickets } from '../../features/tickets/ticketsSlice'
+import ShowMoreBtn from '../ShowMoreBtn/ShowMoreBtn'
 
 import '../../styles/globals.scss'
 import styles from './style.module.scss'
 
 function App() {
   const dispatch = useDispatch()
-  const { searchId, status, error } = useSelector((state) => state.tickets)
+  const { searchId, status, error, ticketsToShow } = useSelector((state) => state.tickets)
 
   useEffect(() => {
     if (!searchId) {
@@ -37,6 +38,8 @@ function App() {
           {error && <div>{`Error: ${error}`}</div>}
 
           <CardList />
+
+          {ticketsToShow.length > 0 && <ShowMoreBtn />}
         </Main>
       </div>
     </div>
---
+++ src/components/ShowMoreBtn/ShowMoreBtn.jsx
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import { selectFilteredTickets } from '../App/selectors'
+import { setTicketsToShow } from '../../features/tickets/ticketsSlice'
+
+import style from './style.module.scss'
+
+function ShowMoreBtn() {
+  const dispatch = useDispatch()
+  const filteredTickets = useSelector(selectFilteredTickets)
+  const { ticketsToShow } = useSelector((state) => state.tickets)
+
+  const loadMoreTickets = () => {
+    dispatch(
+      setTicketsToShow([
+        ...ticketsToShow,
+        ...filteredTickets.slice(ticketsToShow.length - 1, ticketsToShow.length + 5),
+      ]),
+    )
+  }
+
+  return (
+    <div className={style.ShowMoreBtn}>
+      <button className={style.ShowMoreBtn__btn} type="button" onClick={() => loadMoreTickets()}>
+        Показать еще 5 билетов!
+      </button>
+    </div>
+  )
+}
+
+export default ShowMoreBtn
